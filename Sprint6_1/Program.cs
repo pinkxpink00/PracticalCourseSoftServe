3affe325042ce4d09456df752c02c4f347570cb8
pinkxpink00 <kyperu07@gmail.com>
2023-08-23 01:30:20 +0300

    add new task

--- Sprint6_1/Program.cs
+++ Sprint6_1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ShowPower
+{
+	public static IEnumerable<double> SuperPower(int number, int degree)
+	{
+		if (degree == 0)
+		{
+			yield return 1;
+			yield break;
+		}
+		if (number == 0)
+		{
+			yield return 0;
+			yield break;
+		}
+		double result = 1;
+		if (degree > 0)
+		{
+			for(int i =1; i < degree; i++)
+			{
+				result *= number;
+				yield return result;
+			}
+		}
+		if (degree < 0)
+		{
+			for (int i = 1; i < Math.Abs(degree);i--)
+			{
+				result *= number;
+				yield return 1 / result;
+			}
+		}
+	}
+}
---
+++ sprint 6_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+public class PowersOf2
+{
+	static void Main(string[] args)
+	{
+		foreach (int i in Power(2, 5))
+		{
+			Console.WriteLine("{0}", 1);
+		}
+	}
+	public static IEnumerable<int> Power(int number, int exponent)
+	{
+		int result = 1;
+		for (int i = 0; i < exponent; i++)
+		{
+			result = result * number;
+			yield return result;
+		}
+	}
+
+}
