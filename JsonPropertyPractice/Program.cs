69e87c66366c69d3ec7d1629423340450136b082
pinkxpink00 <kyperu07@gmail.com>
2023-08-23 23:37:05 +0300

    practice with property v1

---
+++ JsonPropertyPractice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person tom = new Person("Tom",55);
+
+        string json = JsonSerializer.Serialize(tom);
+        Console.WriteLine(json);
+    }
+}
+
+class Person
+{
+    [JsonPropertyName("firstName")]
+	public string Name {get;set;}
+    [JsonIgnore]
+	public int Age { get; set;}
+    public Person(string name,int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+}
