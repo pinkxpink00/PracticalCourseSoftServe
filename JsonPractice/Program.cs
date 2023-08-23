b0f099713b0943df94bc004c4d0b8db083533935
pinkxpink00 <kyperu07@gmail.com>
2023-08-23 22:32:10 +0300

    json practice

--- JsonPractice/Program.cs
+++ JsonPractice/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args) 
+    {
+        Person tom = new Person("Tom", 24);
+        string json = JsonSerializer.Serialize(tom);
+        Console.WriteLine(json);
+        Person? restoredPerson = JsonSerializer.Deserialize<Person>(json);
+        Console.WriteLine(restoredPerson.Name,restoredPerson.Age);
+    }
+}
+
+class Person
+{
+	public string Name { get; set; }
+	public int Age { get; set; }
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+}
