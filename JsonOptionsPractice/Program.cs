77d4334030bd3191a11edea051edc85845823379
pinkxpink00 <kyperu07@gmail.com>
2023-08-23 23:08:06 +0300

    json options Practice

---
+++ JsonOptionsPractice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person tom = new Person("Mike",23);
+
+        var option = new JsonSerializerOptions()
+        {
+           
+            AllowTrailingCommas = true,
+        };
+
+        string json = JsonSerializer.Serialize(tom, option);
+        Console.WriteLine(json);
+        Person? person = JsonSerializer.Deserialize<Person>(json);
+        Console.WriteLine(person?.Name);
+    }
+}
+
+class Person
+{
+	public string Name { get; set; }
+	public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+}
