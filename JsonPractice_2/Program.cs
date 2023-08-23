4320341874333c5a8db908859bfbf688f8e8af03
pinkxpink00 <kyperu07@gmail.com>
2023-08-23 22:58:58 +0300

    json practice with saved file

---
+++ JsonPractice_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.Json;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		using (FileStream fs = new FileStream("user.json", FileMode.OpenOrCreate))
+		{
+			Person tom = new Person("Tom",53);
+			JsonSerializer.SerializeAsync<Person>(fs, tom);
+            Console.WriteLine("Data has been saved to file");
+        }
+
+		using (FileStream fs = new FileStream("user.json", FileMode.OpenOrCreate))
+		{
+			Person? person = JsonSerializer.Deserialize<Person>(fs);
+			Console.WriteLine($"Name:{person?.Name}, Age: {person?.Age}");
+        }
+	}
+			
+
+
+}
+
+public class Person
+{
+	public string Name { get; set; }
+	public int Age { get; set; }
+    public Person(string name,int age)
+    {
+        this.Name = name;
+		this.Age = age;
+    }
+}
