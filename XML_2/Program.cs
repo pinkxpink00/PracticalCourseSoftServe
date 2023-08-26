005a025e20e1b35dbaf54d1cf645250e454d523e
pinkxpink00 <kyperu07@gmail.com>
2023-08-26 18:10:01 +0300

    xml2

---
+++ XML_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+[Serializable]
+public class Person
+{
+	public string Name { get; set; }
+	public int Age { get; set; }
+    public Person() { }
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+        
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person p1 = new Person("Dobby", 24);
+
+        XmlSerializer xmlSerializer = new XmlSerializer(typeof(Person));
+
+        using (FileStream fs = new FileStream("per.xml", FileMode.OpenOrCreate))
+        {
+            xmlSerializer.Serialize(fs, p1);
+            Console.WriteLine("xml created");
+        }
+
+        using (FileStream fs2 = new FileStream("per.xml", FileMode.Open))
+        {
+            Person? person = xmlSerializer.Deserialize(fs2) as Person;
+            Console.WriteLine($"Name:{person?.Name}, Age: {person?.Age}");
+        }
+    }
+}
