3bd71b20147e5c95fe00d41dda90cb6e5c16d07f
pinkxpink00 <kyperu07@gmail.com>
2023-08-26 18:18:50 +0300

    Linq_1

---
+++ linq_1/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+	static void Main(string[] args)
+	{
+		string[] people = { "Tom", "Bob", "Lorry", "Ted" };
+
+		var selectedPeople = from p in people
+							 where p.ToUpper().StartsWith("T")
+							 orderby p
+							 select p;
+
+		foreach (string person in selectedPeople)
+		{
+            Console.WriteLine(person);
+        }
+	}
+}
