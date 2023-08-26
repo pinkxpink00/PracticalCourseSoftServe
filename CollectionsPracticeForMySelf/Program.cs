1addf637eedd0c93a887bbb11a985003ec91cb84
pinkxpink00 <kyperu07@gmail.com>
2023-08-26 23:30:01 +0300

    use .Add func

---
+++ CollectionsPracticeForMySelf/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+	static void Main(string[] args)
+	{
+		List<Person> list = new List<Person>();
+
+		var employee = new List<string>() { "Tom", "Bob" };
+        var employee2 = new List<string>(employee) { "Sonya" };
+        var mainVar = employee2;
+
+        foreach (var personName in mainVar)
+        {
+            Person person = new Person(personName);
+            list.Add(person);
+            Console.WriteLine(person.Name);
+        }
+    }
+}
+
+class Person
+{
+	public string Name { get; }
+    public Person(string name)
+    {
+        Name = name;
+    }
+}
