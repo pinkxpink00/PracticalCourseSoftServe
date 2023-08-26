beb59c3e7bdedeacbd704f579edbf8c56944ef20
pinkxpink00 <kyperu07@gmail.com>
2023-08-26 18:50:32 +0300

    ext method practice

---
+++ ExtensionsMeth_1/Program.cs
@@ -0,0 +1,20 @@
+﻿public static class StringExt
+{
+	public static string Reverse(this string s)
+	{
+		char[] charArray = s.ToCharArray();
+		Array.Reverse(charArray);
+		return new string(charArray);
+	}
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string original = "Hello World";
+		string reversed = original.Reverse();
+        Console.WriteLine(original);
+		Console.WriteLine(reversed);
+    }
+}
