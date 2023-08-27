67dcafc18fcc872fdf4228ecfb510bfb1508b412
pinkxpink00 <kyperu07@gmail.com>
2023-08-28 00:04:57 +0300

    threading

---
+++ MultiThreading/Program.cs
@@ -0,0 +1,82 @@
+﻿//ExecutionContext: позволяет получить контекст, в котором выполняется поток
+
+//IsAlive: указывает, работает ли поток в текущий момент
+
+//IsBackground: указывает, является ли поток фоновым
+
+//Name: содержит имя потока
+
+//ManagedThreadId: возвращает числовой идентификатор текущего потока
+
+//Priority: хранит приоритет потока - значение перечисления ThreadPriority:
+
+//Lowest
+
+//BelowNormal
+
+//Normal
+
+//AboveNormal
+
+//HighestExecutionContext: позволяет получить контекст, в котором выполняется поток
+
+//IsAlive: указывает, работает ли поток в текущий момент
+
+//IsBackground: указывает, является ли поток фоновым
+
+//Name: содержит имя потока
+
+//ManagedThreadId: возвращает числовой идентификатор текущего потока
+
+//Priority: хранит приоритет потока - значение перечисления ThreadPriority:
+
+//Lowest
+
+//BelowNormal
+
+//Normal
+
+//AboveNormal
+
+//Highest
+
+//ThreadState возвращает состояние потока - одно из значений перечисления ThreadState:
+
+//Aborted: поток остановлен, но пока еще окончательно не завершен
+
+//AbortRequested: для потока вызван метод Abort, но остановка потока еще не произошла
+
+//Background: поток выполняется в фоновом режиме
+
+//Running: поток запущен и работает (не приостановлен)
+
+//Stopped: поток завершен
+
+//StopRequested: поток получил запрос на остановку
+
+//Suspended: поток приостановлен
+
+//SuspendRequested: поток получил запрос на приостановку
+
+//Unstarted: поток еще не был запущен
+
+//WaitSleepJoin: поток заблокирован в результате действия методов Sleep или Join
+
+using System.Threading;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Thread currentThread = Thread.CurrentThread;
+
+        Console.WriteLine($"Name Thread:{currentThread.Name}");
+		currentThread.Name = "Main Thread";
+		Console.WriteLine($"Name Thread:{currentThread.Name}");
+
+        Console.WriteLine($"Thread is Alive: {currentThread.IsAlive}");
+		Console.WriteLine($"ID:{currentThread.ManagedThreadId}");
+		Console.WriteLine($"Current thread priority:{currentThread.Priority}");
+		Console.WriteLine($"Thread status:{currentThread.ThreadState}");
+    }
+}
