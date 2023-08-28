45219fec69ff8b18a20ce4b315d6f4a9315472c6
pinkxpink00 <kyperu07@gmail.com>
2023-08-28 23:07:51 +0300

    task1

---
+++ TaskTPL/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+	static void Main(string[] args)
+	{
+	    Task task = new Task(() => Console.WriteLine("Hello Task"));
+		task.Start();
+		task.Wait();
+
+        Task task1 = Task.Factory.StartNew(() => Console.WriteLine("Hello Factory Task!"));
+		task1.Wait();
+
+		Task task2 = Task.Run(() => Console.WriteLine("Hello task Run "));
+		task2.Wait();
+	}
+
+}
