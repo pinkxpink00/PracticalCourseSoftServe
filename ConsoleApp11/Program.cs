c998a39e0744f74f8700052ffc524dd7390b1dfd
pinkxpink00 <kyperu07@gmail.com>
2023-08-29 23:50:41 +0300

    fix sprint 2

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -1,58 +1,60 @@
-﻿using System.Threading;
-class MyThreads
+﻿//using System.Diagnostics.Metrics;
+
+//We have the class MainThreadProgram. Please create three methods: Calculator, Product, and Sum.
+
+//Method Sum() should ask the user to enter 5 numbers in the form “Enter the 1st number:”, “Enter the 2nd number:” etc.and calculate their sum. After that, it should output the message “Sum is: < sum >”. 
+
+//Method Product() should generate a List of 10 consequent integer numbers from 1 to 10 and calculate their product. Then it should wait for 10 seconds. After that, it should output the message “Product is: < product >”. 
+
+//The Calculator() method should create two threads: productThread and sumThread, and call the Product and Sum methods in appropriate threads. This method should return a tuple of two threads: (sumThread, productThread).
+
+
+//Answer:(penalty regime: 0 %)
+
+class MainThreadProgram
 {
-	public static readonly object Den = new object();
-	public static readonly object Ada = new object();
+	public void Calculator()
+	{
+		Thread productThread = new Thread(Product);
+		Thread sumThread = new Thread(Sum);
+		sumThread.Start();
 
-	public static int n;
-	public static int m;
 
-	public static Thread Thread1 = new Thread(
-		() =>
+	}
+
+	public void Product()
+	{
+		int productResult = 1;
+
+		List<int> numbers = new List<int>();
+		for (int i = 1; i <= 10; i++)
 		{
-			lock (Den)
-			{
-				for (int i = 0; i < 5; i++, n++)
-				{
-					Console.WriteLine("Thread1 n = " + n);
-					
-				}
-			}
-			lock (Ada)
-			{
-				for (int i = 0; i < 5; i++, m++)
-					Console.WriteLine("Thread1 m = " + m);
-				Console.WriteLine("Thread1 success!");
-			}
+			numbers.Add(i);
+			productResult *= i;
 		}
-	);
+		Thread.Sleep(10000); 
+		Console.WriteLine($"Product is: {productResult}");
+	}
 
-	public static Thread Thread2 = new Thread(
-		() =>
+	public void Sum()
+	{
+		string[] numberEnding = { "st", "nd", "rd", "th", "th" };
+		int sum = 0;
+		for (int i = 1; i <= 5; i++)
 		{
-			lock (Ada)
-			{
-				{
-					for (int i = 0; i < 5; i++, m++)
-						Console.WriteLine("Thread2 m = " + m);
-				}
-				
-			}
-			lock (Den)
-			{
-				for (int i = 0; i < 5; i++, n++)
-					Console.WriteLine("Thread2 n = " + n);
-				Console.WriteLine("Thread2 success!");
-			}
+			Console.WriteLine($"Enter the {i}{numberEnding[i - 1]} number: ");
+			int number = Convert.ToInt32(Console.ReadLine());
+			sum += number;
 		}
-	);
+		Console.WriteLine($"Sum is: {sum}");
+	}
 }
 
 class Program
 {
 	static void Main()
 	{
-		MyThreads.Thread1.Start();
-		MyThreads.Thread2.Start();
+	 MainThreadProgram mainThreadProgram = new MainThreadProgram();
+		mainThreadProgram.Calculator();
 	}
 }
