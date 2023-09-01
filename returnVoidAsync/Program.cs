4e75296279e021d4e41cf4a9726269323a836a64
pinkxpink00 <kyperu07@gmail.com>
2023-09-01 22:59:48 +0300

    async voidv2

--- returnVoidAsync/Program.cs
+++ returnVoidAsync/Program.cs
@@ -2,28 +2,14 @@
 {
 	static async Task Main(string[] args)
 	{
-		Account account = new Account();
-		account.Added += PrintAsync;
-
-		account.Put(500);
 
 		await Task.Delay(2500);
+		await PrintAsync("Hello");
 	}
 
-	static async void PrintAsync(object? obj, string message)
+	static async Task PrintAsync(string message)
 	{
 		await Task.Delay(3000);
 		Console.WriteLine(message);  
-	}
-}
-
-class Account
-{
-	int sum = 0;
-	public event EventHandler<string>? Added;
-	public void Put(int sum)
-	{
-		this.sum += sum;
-		Added?.Invoke(this, $"На счёт поступило:{sum}$");
 	}
 }
