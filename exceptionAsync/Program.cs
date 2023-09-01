b5dc85116d9a96c025b1a7668fc92741d646552d
pinkxpink00 <kyperu07@gmail.com>
2023-09-02 00:51:43 +0300

    add ex for async

---
+++ exceptionAsync/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+	static async Task Main(string[] args)
+	{
+		try
+		{
+			await PrintAsync("Hello World");
+			await PrintAsync("Hu");
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex.Message);
+		}
+	}
+
+	static async Task PrintAsync(string message)
+	{
+		if (message.Length < 3)
+		{
+			throw new ArgumentException($"Invalid string length: {message.Length}");
+		}
+		await Task.Delay(1000);
+		Console.WriteLine(message);
+	}
+}
