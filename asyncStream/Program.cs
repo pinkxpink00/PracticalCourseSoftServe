1a5ce3d460cc25825e7a00c04107fb9c33d9c147
pinkxpink00 <kyperu07@gmail.com>
2023-09-02 01:00:21 +0300

    stream v1

---
+++ asyncStream/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+	static async Task Main()
+	{
+		await foreach (var number in GetNumbersAsync())
+		{
+			Console.WriteLine(number);
+		}
+	}
+
+	static async IAsyncEnumerable<int> GetNumbersAsync()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			await Task.Delay(1000);
+			yield return i;
+		}
+	}
+}
