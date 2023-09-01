4612ad87e4d813ac361269bd9ff8374dda4997f0
pinkxpink00 <kyperu07@gmail.com>
2023-09-01 19:28:47 +0300

    async v4

---
+++ AsynchronProg/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+	static void Main()
+	{
+		FactorialAsync();
+        Console.WriteLine("Enter the number:");
+		int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Square equals:{n*n}");
+		Console.ReadLine();
+    }
+
+	static void Factorial()
+	{
+		int result = 1;
+
+		for (int i = 0; i < 6; i++)
+		{
+			result *= i;
+		}
+		Thread.Sleep(8000);
+        Console.WriteLine($"Factorial result:{result}");
+    }
+
+	static async void FactorialAsync()
+	{
+		Console.WriteLine("Begin FactorialAsync");
+		await Task.Run(() => Factorial());
+		Console.WriteLine("End FactorialAsync");
+    }
+}
