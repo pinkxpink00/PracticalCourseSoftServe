09f948c9eea72ad7a67d88bcc896f159b2a5bbe1
pinkxpink00 <kyperu07@gmail.com>
2023-09-01 22:28:32 +0300

    async method example v2

--- Async_v2/Program.cs
+++ Async_v2/Program.cs
@@ -1,15 +1,34 @@
-﻿class Program
+﻿using System.Diagnostics;
+
+class Program
 {
-	static void Main(string[] args)
+	//static void Main(string[] args)
+	//{
+	//	PrintName("German");
+	//	PrintName("Lucas");
+	//	PrintName("Bobo");
+	//}
+
+	//static void PrintName(string name)
+	//{
+	//	Thread.Sleep(3000);
+ //       Console.WriteLine(name);
+ //   }
+
+	static async Task Main()
 	{
-		PrintName("German");
-		PrintName("Lucas");
-		PrintName("Bobo");
+		var germanTask = PrintNameAsync("German");
+	    var lucasTask = PrintNameAsync("Lucas");
+		var boboTask = PrintNameAsync("Bobo");
+
+		await germanTask;
+		await lucasTask;
+		await boboTask;
+	}
+	static async Task PrintNameAsync(string name)
+	{
+		await Task.Delay(2000);
+		Console.WriteLine(name);
 	}
 
-	static void PrintName(string name)
-	{
-		Thread.Sleep(3000);
-        Console.WriteLine(name);
-    }
 }
