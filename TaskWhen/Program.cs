6d561668d3273790e48d09d9b0f48f17436f71a5
pinkxpink00 <kyperu07@gmail.com>
2023-09-01 23:47:21 +0300

    use whenall

---
+++ TaskWhen/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+	static async Task Main()
+	{
+		var german = PrintAsync("German");
+		var jett = PrintAsync("Jett");
+		var luca = PrintAsync("Luca");
+
+		await Task.WhenAll(german, jett, luca);
+	}
+
+	static async Task PrintAsync(string message)
+	{
+		await Task.Delay(1000);
+		Console.WriteLine(message);
+	}
+}
