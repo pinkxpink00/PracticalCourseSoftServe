470e6dde85ec3bdcc5894603b5421867ff6b29bb
pinkxpink00 <kyperu07@gmail.com>
2023-09-01 22:18:41 +0300

    met async v1

---
+++ Async_v2/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+	static async Task Main(string[] args)
+	{
+		await PrintAsync();
+		Console.WriteLine("Some actions in method Main");
+
+    }
+	
+	static void Print()
+	{
+		Thread.Sleep(3000);
+        Console.WriteLine("Hello C#");
+    }
+
+	static async Task PrintAsync()
+	{
+		Console.WriteLine("Start Async");
+		await Task.Run(()=>Print());
+		Console.WriteLine("End Async");
+
+    }
+}
