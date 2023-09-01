730e69ea3502cd58a5aabc646ff735f1bc720c16
pinkxpink00 <kyperu07@gmail.com>
2023-09-01 21:59:03 +0300

    async v5

---
+++ CancellationTokenAsync/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+	static void Main()
+	{
+		CancellationTokenSource cts = new CancellationTokenSource();
+		CancellationToken token = cts.Token;
+		Factorial(6, token);
+		Thread.Sleep(3000);
+		cts.Cancel();
+		Console.Read();
+	}
+
+	static void Factorial(int n, CancellationToken token)
+	{
+		int result = 1;
+
+		for(int i = 0; i <= n; i++)
+		{
+			if (token.IsCancellationRequested)
+			{
+				Console.WriteLine("Aborted");
+				return;
+            }
+			result *= i;
+            Console.WriteLine($"The factorial of {i} equals {result}");
+
+			Thread.Sleep(1000);
+        }
+	}
+
+	static async void FactorialAsync(int n, CancellationToken token)
+	{
+		if (token.IsCancellationRequested)
+		{
+			return;
+		}
+		await Task.Run (()=> Factorial(n, token));
+	}
+}
