ef6f4cc2aef0137622069ee3ba620213477c0303
pinkxpink00 <kyperu07@gmail.com>
2023-09-05 19:32:35 +0300

    fix some bug

--- SprintAsync_2/Program.cs
+++ SprintAsync_2/Program.cs
@@ -14,47 +14,30 @@ public static class CalcAsync
 {
 	public static async Task TaskPrintSeqAsync(int n)
 	{
-		try
-		{
-			int result = await Task.Run(() => Calc.Seq(n));
-			Console.WriteLine($"Seq[{n}] = {result}");
-		}
-		catch (Exception ex)
-		{
-			Console.WriteLine($"Error: {ex.Message}");
-		}
+		await Task.Run(() => Console.WriteLine("Seq[" + n + "] = " + Calc.Seq(n)));
 	}
 
 	public static void PrintStatusIfChanged(this Task task, ref TaskStatus previousStatus)
 	{
-		if (task == null)
-			throw new ArgumentNullException(nameof(task));
-
 		if (task.Status != previousStatus)
 		{
 			Console.WriteLine(task.Status);
 			previousStatus = task.Status;
-			
 		}
 	}
 
-	public static async void TrackStatus(this Task task)
+	public static void TrackStatus(this Task task)
 	{
-		if (task == null)
-			throw new ArgumentNullException(nameof(task));
-
 		TaskStatus previousStatus = TaskStatus.Created;
 
 		while (!task.IsCompleted)
 		{
 			task.PrintStatusIfChanged(ref previousStatus);
-			await Task.Delay(100);
 		}
 
 		task.PrintStatusIfChanged(ref previousStatus);
 	}
 }
-
 class Program
 {
 	static async Task Main(string[] args)
--- SprintAsync_4/Program.cs
+++ SprintAsync_4/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+class Calc
+{
+	public static int Seq(int n)
+	{
+		return n * n;
+	}
+}
+
+class CalcAsync
+{
+	async IAsyncEnumerable<int> SeqStreamAsync(int n)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			await Task.Delay(100);
+			yield return i;
+		}
+	}
+
+	public static async Task PrintStreamAsync(IAsyncEnumerable<(int, int)> stream)
+	{
+		await foreach (var item in stream)
+		{
+			Console.WriteLine($"Seq[{item.Item1}] = {item.Item2}");
+		}
+	}
+}
