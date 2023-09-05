0ac7655b793ccd721e5009cc18177af06733e3a3
pinkxpink00 <kyperu07@gmail.com>
2023-09-05 22:56:53 +0300

    add DIP

---
+++ SOLID_DIP/Program.cs
@@ -0,0 +1,64 @@
+﻿//class Book
+//{
+//	public string Text { get; set; }
+//	public ConsolePrinter Printer { get; set; }
+
+//	public void Print()
+//	{
+//		Printer.Print(Text);
+//	}
+//}
+
+//class ConsolePrinter
+//{
+//	public void Print(string text)
+//	{
+//		Console.WriteLine(text);
+//	}
+//}
+
+
+interface IPrinter
+{
+	void Print(string text);
+}
+class Book
+{
+	public string Text { get; set; }
+	public IPrinter Printer { get; set; }
+	public Book(IPrinter printer)
+	{
+		this.Printer = printer;
+	}
+	public void Print()
+	{
+		Printer.Print(Text);
+	}
+}
+
+class ConsolePrinter : IPrinter
+{
+	public void Print(string text)
+	{
+        Console.WriteLine("Print into console...");
+    }
+}
+
+class HtmlPrinter : IPrinter
+{
+	public void Print(string text)
+	{
+        Console.WriteLine("Print into HTML");
+    }
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Book book = new Book(new ConsolePrinter());
+		book.Print();
+		book.Printer = new HtmlPrinter();
+		book.Print();
+	}
+}
--- SOLID_ISP/Program.cs
+++ SOLID_ISP/Program.cs
@@ -1,2 +1,90 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿interface IMessage
+{
+	void Send();
+	string ToAddress { get; set; }
+	string FromAddress { get; set; }
+}
+interface IVoiceMessage : IMessage
+{
+	byte[] Voice { get; set; }
+}
+interface ITextMessage : IMessage
+{
+	string Text { get; set; }
+}
+
+interface IEmailMessage : ITextMessage
+{
+	string Subject { get; set; }
+}
+
+class VoiceMessage : IVoiceMessage
+{
+	public string ToAddress { get; set; } = "";
+	public string FromAddress { get; set; } = "";
+
+	public byte[] Voice { get; set; } = Array.Empty<byte>();
+	public void Send() => Console.WriteLine("Передача голосовой почты");
+}
+class EmailMessage : IEmailMessage
+{
+	public string Text { get; set; } = "";
+	public string Subject { get; set; } = "";
+	public string FromAddress { get; set; } = "";
+	public string ToAddress { get; set; } = "";
+
+	public void Send() => Console.WriteLine($"Отправляем по Email сообщение: {Text}");
+}
+
+class SmsMessage : ITextMessage
+{
+	public string Text { get; set; } = "";
+	public string FromAddress { get; set; } = "";
+	public string ToAddress { get; set; } = "";
+	public void Send() => Console.WriteLine($"Отправляем по Sms сообщение: {Text}");
+}
+
+
+
+
+
+// Плохой пример
+
+public interface IWorker
+{
+	void Work();
+	void Eat();
+}
+
+public class Robot : IWorker
+{
+	public void Work()
+	{
+		// Работа робота
+	}
+
+	public void Eat()
+	{
+		// Робот не ест, но он должен реализовать этот метод из-за интерфейса
+	}
+}
+
+// Хороший пример
+
+public interface IWorker
+{
+	void Work();
+}
+
+public interface IEater
+{
+	void Eat();
+}
+
+public class Robot : IWorker
+{
+	public void Work()
+	{
+		// Работа робота
+	}
+}
