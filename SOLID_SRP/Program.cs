2a346759ed34a9a0c976de1c4c0323744eea0826
pinkxpink00 <kyperu07@gmail.com>
2023-09-05 19:45:30 +0300

    add main point

--- SOLID_SRP/Program.cs
+++ SOLID_SRP/Program.cs
@@ -1,6 +1,8 @@
-﻿using System.Reflection;
+﻿using System.Net.Http.Headers;
+using System.Reflection;
 using System.Security.Principal;
 using System.Threading.Channels;
+using System;
 
 //Single Responsibility Principle (Принцип единственной обязанности)
 
@@ -31,63 +33,100 @@ class Phone
         Model = model;
         Price = price;
     }
+}
 
-    class MobileStore
-    {
-        List<Phone> phones = new List<Phone>();
-		public IPhoneReader Reader { get; set; }
-		public IPhoneBinder 
-    }
-	interface IPhoneReader
+
+class MobileStore
+{
+	List<Phone> phones = new List<Phone>();
+	public IPhoneReader Reader { get; set; }
+	public IPhoneBinder Binder { get; set; }
+	public IPhoneValidator Validator { get; set; }
+	public IPhoneSaver Saver { get; set; }
+
+	public MobileStore(IPhoneReader reader, IPhoneBinder binder, IPhoneValidator validator, IPhoneSaver saver)
 	{
-		string?[] GetInputData();
+		this.Reader = reader;
+		this.Binder = binder;
+		this.Validator = validator;
+		this.Saver = saver;
 	}
-	class ConsolePhoneReader : IPhoneReader
+	public void Process()
 	{
-		public string?[] GetInputData()
+		string?[] data = Reader.GetInputData();
+		Phone phone = Binder.CreatePhone(data);
+		if (Validator.IsValid(phone))
 		{
-			Console.WriteLine("Input model:");
-			string? model = Console.ReadLine();
-			Console.WriteLine("Input price:");
-			string? price = Console.ReadLine();
-			return new string?[] { model, price };
+			phones.Add(phone);
+			Saver.Save(phone, "store.txt");
+			Console.WriteLine("Data have done");
+		}
+		else
+		{
+			Console.WriteLine("Incorrect data");
 		}
 	}
-	interface IPhoneBinder
+}
+interface IPhoneReader
+{
+	string?[] GetInputData();
+}
+class ConsolePhoneReader : IPhoneReader
+{
+	public string?[] GetInputData()
 	{
-		Phone CreatePhone(string?[] data);
+		Console.WriteLine("Input model:");
+		string? model = Console.ReadLine();
+		Console.WriteLine("Input price:");
+		string? price = Console.ReadLine();
+		return new string?[] { model, price };
 	}
-	class GeneralPhoneBuilder : IPhoneBinder
+}
+interface IPhoneBinder
+{
+	Phone CreatePhone(string?[] data);
+}
+class GeneralPhoneBinder : IPhoneBinder
+{
+	public Phone CreatePhone(string?[] data)
 	{
-		public Phone CreatePhone(string?[] data)
+		if (data is { Length: 2 } && data[0] is string model &&
+			model.Length > 0 && int.TryParse(data[1], out var price))
 		{
-			if (data is { Length: 2 } && data[0] is string model &&
-				model.Length > 0 && int.TryParse(data[1], out var price))
-			{
-				return new Phone (model, price);
-			}
-			throw new Exception("Incorrect data");
+			return new Phone(model, price);
 		}
+		throw new Exception("Incorrect data");
 	}
-	interface IPhoneValidator
+}
+interface IPhoneValidator
+{
+	bool IsValid(Phone phone);
+}
+class GeneralPhoneValidator : IPhoneValidator
+{
+	public bool IsValid(Phone phone) => !string.IsNullOrEmpty(phone.Model) && phone.Price > 0;
+}
+interface IPhoneSaver
+{
+	void Save(Phone phone, string fileName);
+}
+class TextPhoneSaver : IPhoneSaver
+{
+	public void Save(Phone phone, string fileName)
 	{
-		bool IsValid(Phone phone);
+		using StreamWriter writer = new StreamWriter(fileName, true);
+		writer.WriteLine(phone.Model);
+		writer.WriteLine(phone.Price);
 	}
-	class GeneralPhoneValidator : IPhoneValidator
+}
+
+class Program
+{
+	static void Main(string[] args)
 	{
-		public bool IsValid(Phone phone) => !string.IsNullOrEmpty(phone.Model) && phone.Price > 0;
-	}
-	interface IPhoneSaver
-	{
-		void Save(Phone phone,string fileName);
-	}
-	class TextPhoneSaver : IPhoneSaver
-	{
-		public void Save(Phone phone, string fileName)
-		{
-			using StreamWriter writer = new StreamWriter(fileName,true);
-			writer.WriteLine(phone.Model);
-			writer.WriteLine(phone.Price);
-		}
+		MobileStore store = new MobileStore(
+	new ConsolePhoneReader(), new GeneralPhoneBinder(),
+	new GeneralPhoneValidator(), new TextPhoneSaver());
+		store.Process();
 	}
 }
