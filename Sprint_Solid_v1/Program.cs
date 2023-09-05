cc11f1a1259b845a0c343c64fc3e8e2068a15e70
pinkxpink00 <kyperu07@gmail.com>
2023-09-06 01:16:13 +0300

    sprintv2 done

--- Sprint_Solid_v1/Program.cs
+++ Sprint_Solid_v1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Customer
+{
+	public void Register(string email, string password)
+	{
+		MailService ms = new MailService();
+			if (ms.ValidEmail(email))
+			{
+			  ms.SendEmail(email, "Email title", "Email body");
+			}
+	}
+}
+
+public class MailService
+{
+	public  void SendEmail(string mail, string emailTitle, string emailBody)
+	{
+		if (ValidEmail(mail))
+		{
+			Console.WriteLine($"Mail:{mail}, Title:{emailTitle}, Body:{emailBody}");
+		}
+	}
+
+	public  bool ValidEmail(string email)
+	{
+		return email.Contains("@");
+	}
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		MailService ms = new MailService();
+		ms.SendEmail("myemail@net.ua", "Say Hello!", "Hello, my dear ..");
+	}
+}
---
+++ sprintSOLID_v2/Program.cs
@@ -0,0 +1,57 @@
+﻿public class Customer
+{
+	public static void Register(string email, string password)
+	{
+		MailService ms = new MailService();
+			if (ms.ValidEmail())
+			{
+			  ms.SendEmail(email, "Email title", "Email body");
+			}
+	}
+}
+public abstract class NotificationService
+{
+	public abstract void SendNotification();
+}
+
+public class MailService : NotificationService
+{
+	public string Email { get; set; }
+	public string EmailTitle { get; set; }
+	public string EmailBody { get; set; }
+	public void SendEmail(string mail, string emailTitle, string emailBody)
+	{
+		if (ValidEmail())
+		{
+			Console.WriteLine($"Mail:{mail}, Title:{emailTitle}, Body:{emailBody}");
+		}
+	}
+	public bool ValidEmail()
+	{
+		return Email.Contains("@");
+	}
+
+	public override void SendNotification()
+	{
+			Console.WriteLine($"Mail:{Email}, Title:{EmailTitle}, Body:{EmailBody}");
+	}
+}
+
+public class SmsService : NotificationService
+{
+	public string Number { get; set; }
+	public string SmsText { get; set; }
+
+	public override void SendNotification()
+	{
+        Console.WriteLine("Number:{0}, Message:{1}", Number, SmsText);
+    }
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Customer.Register("myemail@ukr.ua", "12345");
+	}
+}
