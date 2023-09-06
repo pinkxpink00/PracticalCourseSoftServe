a8b53683b571a28c804c899855f41424d7238fd6
pinkxpink00 <kyperu07@gmail.com>
2023-09-06 23:41:34 +0300

    start learn Reflection

---
+++ Reflectionv1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+        Type myType = typeof(Person);
+        Console.WriteLine(myType.FullName);
+    }
+}
+class Person
+{
+	string Name { get; }
+
+    public Person(string name)
+    {
+        this.Name = name;
+    }
+}
