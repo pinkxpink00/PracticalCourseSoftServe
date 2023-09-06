99045626a5028da63a117b2d3ccf24cce9f28621
pinkxpink00 <kyperu07@gmail.com>
2023-09-06 23:55:56 +0300

    realized intetface

--- Reflectionv1/Program.cs
+++ Reflectionv1/Program.cs
@@ -5,23 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Type myType = typeof(PeopleTypes.Person);
-        Console.WriteLine($"Full Name:{myType.FullName}");
-        Console.WriteLine($"Name:{myType.Name}");
-        Console.WriteLine($"Namespace:{myType.Namespace}");
-        Console.WriteLine($"IsStruct:{myType.IsValueType}");
-        Console.WriteLine($"IsClass:{myType.IsClass}");
+
+        Console.WriteLine("Realized Interface");
+        foreach (Type t in myType.GetInterfaces())
+        {
+            Console.WriteLine(t.Name);
+        }
 
     }
 }
 namespace PeopleTypes
 {
-    class Person
+    interface IEat
+    {
+        void Eat();
+    }
+    interface IMove
+    {
+        void Move();
+    }
+
+    class Person : IEat, IMove
     {
         string Name { get; }
 
         public Person(string name)
         {
             this.Name = name;
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("I Eat");
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("I Move");
         }
     }
 }
