a002a0dc5b8d4153c22da7a01ab499ef545536bd
pinkxpink00 <kyperu07@gmail.com>
2023-09-07 00:22:03 +0300

    use System.Reflection MemberInfo

---
+++ Applying reflection and exploring types/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Channels;
+using System.Reflection;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Type myType = typeof(Person);
+
+        foreach (MemberInfo memberInfo in myType.GetMembers())
+        {
+            Console.WriteLine($"{memberInfo.DeclaringType} {memberInfo.MemberType} {memberInfo.Name}");
+        }
+    }
+}
+
+class Person
+{
+	public string Name { get;}
+	public int Age { get; set; }
+
+    public Person(string Name, int Age)
+    {
+        this.Name = Name;
+        this.Age = Age;
+    }
+
+    public void Print() => Console.WriteLine($"Name:{Name},Age:{Age}");
+}
