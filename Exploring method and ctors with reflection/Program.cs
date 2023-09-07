70fc56f7efc584050f8a482b333f4ce507e027ed
pinkxpink00 <kyperu07@gmail.com>
2023-09-07 14:16:55 +0300

    exploring method

---
+++ Exploring method and ctors with reflection/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Security;
+using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Type myType = typeof(Printer);
+
+        Console.WriteLine("Methods...");
+
+        foreach(MethodInfo methodInfo in myType.GetMethods())
+        {
+            string modificator = "";
+
+            if (methodInfo.IsStatic) modificator += "static";
+            if (methodInfo.IsVirtual) modificator += "virtual";
+
+            Console.WriteLine($"{modificator} {methodInfo.ReturnType.Name} {methodInfo.Name} ()");
+        }
+    }
+}
+
+class Printer
+{
+    string DefaultMessage { get; set; } = "Hello";
+
+    public void PrintMessage(string message, int times = 1)
+    {
+        while (times-- > 0)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    public string CreateMessage() => DefaultMessage;
+}
