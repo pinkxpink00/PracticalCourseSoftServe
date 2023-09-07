76e92de0388f402fa623004cfbbfafef351673f6
pinkxpink00 <kyperu07@gmail.com>
2023-09-07 16:46:01 +0300

    get status prop

---
+++ PropertiesReflection/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Type type = typeof(Person);
+
+        foreach(PropertyInfo prop in type.GetProperties(BindingFlags.Instance|BindingFlags.Public|BindingFlags.Static))
+        {
+            Console.WriteLine($"{prop.PropertyType} {prop.Name}");
+            if (prop.CanRead)
+            {
+                Console.WriteLine("get;");
+            }
+            if(prop.CanWrite)
+            {
+                Console.WriteLine("set;");
+            }
+        }
+    }
+}
+
+class Person
+{
+	public string Name { get; set; }
+	public int Age { get; }
+    public Person(string name,int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
