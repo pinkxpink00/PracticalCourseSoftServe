01711b520364eee265fe5292a55cb0113d8e408b
pinkxpink00 <kyperu07@gmail.com>
2023-09-07 15:35:23 +0300

    add last change

--- SOLIDSPRINTv7/Program.cs
+++ SOLIDSPRINTv7/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Mail;
+interface ISender
+{
+	void SendEmail(string text);
+}
+
+interface IFileLogger
+{
+	public void Check();
+
+	public void Debug();
+
+	public void Info();
+	
+	
+}
+
+public class FileLogger
+{
+	public void Check()
+	{
+
+	}
+	public void Debug()
+	{
+
+	}
+	public void Info()
+	{
+
+	}
+}
+
+public class MainSender : ISender
+{
+	public void SendEmail(string mailMessage)
+	{
+		Console.WriteLine("Sending Email: {0}", mailMessage);
+	}
+}
+
+public class Invoice
+{
+	public string From { get; set; }
+	public string To { get; set; }
+	public long Amount { get; set; }
+	public DateTime InvoiceDate { get; set; }
+
+	public void Add()
+	{
+		MainSender mS = new MainSender();
+		Console.WriteLine("Adding amount...");
+		string mailMessage = "Your invoice is ready.";
+		mS.SendEmail(mailMessage);
+
+	}
+
+	public void Delete()
+	{
+		Console.WriteLine("Deleting amount...");
+
+	}
+}
+
+
--- SOLID_Sprintv6/Program.cs
+++ SOLID_Sprintv6/Program.cs
@@ -1,2 +1,132 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿interface IFlyable
+{
+	void Fly();
+}
+
+interface IEating
+{
+	void Eat();
+}
+
+interface IMoving
+{
+	void Move();
+}
+
+interface IBasking
+{
+	void Bask();
+}
+
+interface IKryaking
+{
+	void Krya();
+}
+
+
+class Bird : IFlyable, IEating, IMoving, IBasking, IKryaking
+{
+	public void Fly()
+	{
+		Console.WriteLine("I believe, I can fly");
+	}
+
+	public void Eat()
+	{
+		Console.WriteLine("Oh! My corn!");
+	}
+
+	public void Move()
+	{
+		Console.WriteLine("I can jump!");
+	}
+
+	public void Bask()
+	{
+		Console.WriteLine("Bird likes to bask in the sun.");
+	}
+
+	public void Krya()
+	{
+		Console.WriteLine("Bird makes a Krya sound.");
+	}
+}
+
+class Cat : IEating, IMoving, IBasking, IKryaking
+{
+	public void Eat()
+	{
+		Console.WriteLine("Oh! My milk!");
+	}
+
+	public void Move()
+	{
+		Console.WriteLine("I can jump!");
+	}
+
+	public void Bask()
+	{
+		Console.WriteLine("Mrrr-Mrrr-Mrrr...");
+	}
+
+	public void Krya()
+	{
+		Console.WriteLine("Cat makes a Krya sound.");
+	}
+}
+
+class Parrot : Bird
+{
+	public static void Fly()
+	{
+		Console.WriteLine("I believe, I can fly");
+	}
+
+	public static void Eat()
+	{
+		Console.WriteLine("Oh! My seeds and fruits!");
+	}
+
+	public static void Move()
+	{
+		Console.WriteLine("I can jump!");
+	}
+
+	public static void Bask()
+	{
+		Console.WriteLine("Chuh-Chuh-Chuh...");
+	}
+
+	public  static void Krya()
+	{
+		Console.WriteLine("Krya-Krya-Krya...");
+	}
+}
+
+class Duck : IFlyable, IEating, IMoving, IBasking, IKryaking
+{
+	public void Fly()
+	{
+		Console.WriteLine("I believe, I can fly");
+	}
+
+	public void Eat()
+	{
+		Console.WriteLine("Oh! My corn!");
+	}
+
+	public void Move()
+	{
+		Console.WriteLine("I can swimm!");
+	}
+
+	public void Bask()
+	{
+		Console.WriteLine("Duck likes to bask in the water.");
+	}
+
+	public void Krya()
+	{
+		Console.WriteLine("Krya-Krya!");
+	}
+}
