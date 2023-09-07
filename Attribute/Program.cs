aca84b9228a30843ac9a3800df2899a47093bceb
pinkxpink00 <kyperu07@gmail.com>
2023-09-07 17:14:54 +0300

    use attribute

---
+++ Attribute/Program.cs
@@ -0,0 +1,48 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+		Person tom = new Person("Tom", 35);
+		Person bob = new Person("Bob", 16);
+		bool tomIsValid = ValidateUser(tom);    // true
+		bool bobIsValid = ValidateUser(bob);    // false
+
+		Console.WriteLine($"Результат валидации Тома: {tomIsValid}");
+		Console.WriteLine($"Результат валидации Боба: {bobIsValid}");
+	}
+
+	static bool ValidateUser(Person person)
+	{
+        Type type = typeof(Person);
+        object[] attributes = type.GetCustomAttributes(false);
+
+        foreach (Attribute attr in attributes)
+        {
+            if (attr is AgeVallidationAttribute ageAttribute)
+            {
+                return person.Age >=ageAttribute.Age;
+            }
+        }
+
+        return true;
+	}
+}
+
+class AgeVallidationAttribute : Attribute
+{
+	public int Age { get; }
+    public AgeVallidationAttribute(){ }
+	public AgeVallidationAttribute(int age) { Age = age; }
+}
+
+[AgeVallidation(18)]
+class Person
+{
+	public string Name { get; set; }
+	public int Age { get; set; }
+    public Person(string name,int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
