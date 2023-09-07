e00473102aa9a91e8bf7695b953f641cc503e162
pinkxpink00 <kyperu07@gmail.com>
2023-09-07 15:30:15 +0300

    use invoker

---
+++ InvokeReflection/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var myPrinter = new Printer("Hello");
+
+        var print = typeof(Printer).GetMethod("Print");
+
+        print?.Invoke(myPrinter,parameters: null);
+    }
+}
+
+class Printer
+{
+	public string Text { get; }
+    public Printer(string text)
+    {
+        this.Text = text;
+    }
+    public void Print() => Console.WriteLine(Text);
+}
